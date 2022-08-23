d8232950d9f367bceae3ecd6c00523d14046f456
darknessInMe <vladyslav.didukh.22@gmail.com>
2022-08-23 15:05:06 +0300

    prepare for deploy

---
+++ server/data.js
@@ -0,0 +1,9 @@
+const data = [
+    'https://wallpaperaccess.com/full/3567970.jpg',
+    'https://external-preview.redd.it/ad5TBwGIhvfi2j_wCrao1giqZS90uKp4lZ7iTBrxC70.jpg?auto=webp&s=04066b9ec1480198969ed50ddb721ac41a3b75dc',
+    'https://images7.alphacoders.com/493/493639.jpg',
+    'https://www.teahub.io/photos/full/8-86540_dark-souls-12160-src-vertical-dark-souls-wallpapers.jpg',
+    'https://images3.alphacoders.com/669/669303.png',
+];
+
+module.exports = data;
---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const data = require('./data');
+const fetch = require('node-fetch');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// static resources should just be served as they are
+app.use(express.static(
+    path.resolve(__dirname, '..', 'build'),
+    { maxAge: '30d' },
+));
+
+app.listen(PORT, (error) => {
+    if (error) return console.log('Error during app startup', error);
+
+    console.log(`listening on ${PORT}...`);
+});
+
+const indexPath  = path.resolve(__dirname, '..', 'build', 'index.html');
+app.get('/posts/:id', (req, res) => {
+    fs.readFile(indexPath, 'utf8', async (err, htmlData) => {
+        if (err) {
+            console.error('Error during file reading', err);
+            return res.status(404).end()
+        }
+
+        const postId = req.params.id;
+
+        const post = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+            .then(response => response.json())
+
+        htmlData = htmlData.replace(
+            '<title>React App</title>',
+            `<title>${post.title}</title>`
+        )
+        .replace('__META_OG_TITLE__', post.title)
+        .replace('__META_OG_DESCRIPTION__', post.body)
+        .replace('__META_DESCRIPTION__', post.body)
+        .replace('__META_OG_IMAGE__', data[postId])
+        .replace('__META_SECURE_OG_IMAGE__', data[postId])
+
+        return res.send(htmlData);
+    });
+});
